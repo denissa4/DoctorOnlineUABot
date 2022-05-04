f15959bd8bf72f64e3ca3e3a348d94194e24eb0f
Anton Kotseruba <ghost8recon@gmail.com>
2022-05-04 18:41:11 +0300

    [main] add errors in log

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def on_error(context: TurnContext, error: Exception):
     # This check writes out errors to console log .vs. app insights.
     # NOTE: In production environment, you should consider logging this to Azure
     #       application insights.
-    print(f"\n [on_turn_error] unhandled error: {error}", file=sys.stderr)
+    print(f"\n [on_turn_error] unhandled error: {error}")
     traceback.print_exc()
 
     # Send a message to the user
