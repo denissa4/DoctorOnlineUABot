54905e86680d5790aef65f0ae2fbf0f1b34a4acb
Anton Kotseruba <ghost8recon@gmail.com>
2022-05-09 17:28:11 +0300

    [main] change error message

--- main.py
+++ main.py
@@ -38,10 +38,7 @@ async def on_error(context: TurnContext, error: Exception):
     traceback.print_exc()
 
     # Send a message to the user
-    await context.send_activity("The bot encountered an error or bug.")
-    await context.send_activity(
-        "To continue to run this bot, please fix the bot source code."
-    )
+    await context.send_activity("Something went wrong. Please try again.")
 
     # Send a trace activity if we're talking to the Bot Framework Emulator
     if context.activity.channel_id == "emulator":
